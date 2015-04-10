832b15b8d5b6d90d824a2f2d344a3a225619390c
yuliastitarigan <asti.tarigan2@gmail.com>
2015-04-10 16:20:53 +0700

    Do : Mengerjakan Tindakan_Dokter_Service.java dan Tindakan_Periksa_Service.java
    Obstacle : -
    
    To do : Mengerjakan Coding Menginput tindakan medis

---
+++ Project SI_Klinik/SI_Klinik_API/src/database/Service/Tindakan_Dokter_Service.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.Service;
+import database.entity.Tindakan_Dokter;
+import database.entity.petugas;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+/**
+ *
+ * @author Acer
+ */
+public interface Tindakan_Dokter_Service extends Remote {
+    Tindakan_Dokter insertTindakan_Dokter(Tindakan_Dokter dokter) throws RemoteException;
+
+    void updateTindakan_Dokter(Tindakan_Dokter dokter) throws RemoteException;
+
+    Tindakan_Dokter getTindakan_Dokter (String Tindakan_Dokter) throws RemoteException;
+
+}
---
+++ Project SI_Klinik/SI_Klinik_API/src/database/Service/Tindakan_Periksa_Service.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database.Service;
+import database.entity.Tindakan_Periksa ;
+import database.entity.petugas;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+
+/**
+ *
+ * @author Acer
+ */
+public interface Tindakan_Periksa_Service extends Remote {
+   Tindakan_Periksa insertTindakan_Periksa(Tindakan_Periksa periksa) throws RemoteException;
+
+    void updateTindakan_Periksa(Tindakan_Periksa periksa) throws RemoteException;
+
+    Tindakan_Periksa getTindakan_Periksa (String Tindakan_Periksa) throws RemoteException;
+
+    
+}
